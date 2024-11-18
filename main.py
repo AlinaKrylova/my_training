0c0e47edfe12e7bada03db1b2ce52ab4cc35e29b
AlinaKrylova <krylova.alina.romanovna@gmail.com>
2024-11-18 20:28:02 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print ("Hello World")
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3nd program
+a = 2 * 2 + 2
+print(a)
+b = (2 * ( 2 + 2 ))
+print(b)
+print( a == b )
+
+# 4th program
+aa = 123.456
+print ( int (aa % 1 * 10))
 
