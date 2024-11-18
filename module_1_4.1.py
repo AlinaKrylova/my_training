ea782eb83466969f7c42d0a1a087bce0b0363d7a
AlinaKrylova <krylova.alina.romanovna@gmail.com>
2024-11-18 21:12:09 +0500

    Add files via upload

---
+++ module_1_4.1.py
@@ -0,0 +1,8 @@
+my_string = input('Введите текст: ')
+print(len(my_string))
+print(my_string.upper()) # Верхний регистр
+print(my_string.lower()) # Нижний регистр
+print(my_string.replace(' ','')) # Удаляем пробелы
+print(my_string [0]) #Вывод певрого символа
+print(my_string [len(my_string)-1]) #Вывод последнего символа
+
