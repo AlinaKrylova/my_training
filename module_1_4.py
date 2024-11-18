0209241d37845d5b5df0c82af9456af74353b977
AlinaKrylova <krylova.alina.romanovna@gmail.com>
2024-11-18 20:24:06 +0500

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+completed_hw = 12
+hours_spent = 1.5
+course = 'Python'
+time_to_one_task = hours_spent / completed_hw
+print ('Курс:',course, 'всего задач:', completed_hw,
+       'затрачено часов:', hours_spent,
+       'среднее время выполнения', time_to_one_task, 'часа.')
