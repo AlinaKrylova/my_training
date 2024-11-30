dc39895649a77d7dcfbdf19c64a333eda7f2af5b
AlinaKrylova <krylova.alina.romanovna@gmail.com>
2024-11-30 18:42:17 +0500

    Add files via upload

---
+++ module_1_Z.py
@@ -0,0 +1,27 @@
+Students = input('Введите имена учеников через запятую: ')
+Students = Students.replace(' ','').split(',')
+Students = sorted(Students)
+print()
+
+
+
+List_= {}
+
+print('Введите оценки через запятую:')
+
+for item in Students:
+    Grades = []
+    Average_Grades = []
+    Grades += (input(item + " -> ",  )).split(',')
+    Average_Grades +=  [round(sum(int(s) for s in Grades if s.isdigit()) / len(Grades),2)]
+    print(Average_Grades)
+    List_.update({item: float(s) for s in Average_Grades})
+
+print(List_)
+
+
+
+
+
+
+
